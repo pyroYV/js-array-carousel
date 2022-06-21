0fbc569355a4ffe6e1b5826674ca5031b8059af4
pyroYV <simonerizzo.pvt@gmail.com>
2022-06-21 18:17:59 +0200

    milestone 2

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-console.log('itworks')
-
-let carouselIndex = 0
-
 const arrayImg = ['https://cdn.photographycourse.net/wp-content/uploads/2022/04/Portrait-vs-Landscape-Featured-Image-3.jpg',
 'https://i.natgeofe.com/n/2a832501-483e-422f-985c-0e93757b7d84/6.jpg',
 'https://cdn.photographycourse.net/wp-content/uploads/2014/11/Landscape-Photography-steps.jpg',
@@ -9,18 +5,35 @@ const arrayImg = ['https://cdn.photographycourse.net/wp-content/uploads/2022/04/
 'https://iso.500px.com/wp-content/uploads/2021/02/Torres-Del-Paine-Sunset-By-Paul-Reiffer-2-1500x1000.jpg',
 'https://mymodernmet.com/wp/wp-content/uploads/2020/02/Landscape-Photographer-of-the-Year-Sander-Grefte.jpg'];
 
-
+let carouselIndex = 1;
 const carouselWrapper = document.getElementById('carousel-wrapper');
 const imgList = carouselWrapper.children;
 
 for (let index = 0; index < arrayImg.length; index++) {
-    
     const newImg = document.createElement('img');
     newImg.classList.add('w-100','img-fix',);
     newImg.setAttribute('src',arrayImg[index]);
     carouselWrapper.append(newImg);
-    if(carouselIndex===index){
-        newImg.classList.add('active')
-    }
-    
 }
+
+imgList[carouselIndex].classList.add('active')
+const nextButton = document.getElementById('next');
+const backButton = document.getElementById('back');
+
+nextButton.addEventListener('click', function(){
+   imgList[carouselIndex].classList.remove('active');
+    carouselIndex++;
+        if(carouselIndex === arrayImg.length){
+            carouselIndex = 0;
+        }
+        imgList[carouselIndex].classList.add('active');
+})
+
+back.addEventListener('click', function(){
+    imgList[carouselIndex].classList.remove('active');
+     carouselIndex--;
+         if(carouselIndex === -1 ){
+             carouselIndex = arrayImg.length-1;
+         }
+         imgList[carouselIndex].classList.add('active');
+ })
